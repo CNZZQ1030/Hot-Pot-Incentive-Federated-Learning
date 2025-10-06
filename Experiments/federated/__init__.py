88d1d8f34aa5a020d1ba36e0cde7645e750df950
Ziqian Zeng <75361481+CNZZQ1030@users.noreply.github.com>
2025-10-06 20:43:04 +0100

    Delete Experiments directory

--- Experiments/federated/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-federated/__init__.py
-"""
-from .client import FederatedClient
-from .server import FederatedServer
-
-__all__ = ['FederatedClient', 'FederatedServer']
--- Experiments/models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-models/__init__.py
-"""
-from .cnn_model import SimpleCNN, CIFARCNN, ModelFactory
-
-__all__ = ['SimpleCNN', 'CIFARCNN', 'ModelFactory']
-
